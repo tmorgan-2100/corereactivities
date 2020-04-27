c5a474da9e1260097d9673ebd02969a980383bbb
sjm1012 <sjm1012@wildcats.unh.edu>
2020-04-27 12:29:52 -0400

    Added axios, semantic ui

--- API/Startup.cs
+++ API/Startup.cs
@@ -27,8 +27,15 @@ namespace API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+
             services.AddDbContext<DataContext>(opt => {
                 opt.UseSqlite(Configuration.GetConnectionString("DefaultConnection"));
+            });
+            services.AddCors(options => {
+                options.AddPolicy("CorsPolicy", policy =>
+                    {
+                        policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:3000");
+                    });
             });
             services.AddControllers();
         }
@@ -47,10 +54,13 @@ namespace API
 
             app.UseAuthorization();
 
+            app.UseCors("CorsPolicy");
+
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
             });
+
         }
     }
 }
