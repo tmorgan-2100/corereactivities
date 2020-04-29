80aa08ba5a180eaa4c8c54db15d3341842735f55
tmorgan-2100 <tmorgan_2100@yahoo.com>
2020-04-29 08:00:38 -0400

    Module 4 halfway point (getting error)

--- API/Startup.cs
+++ API/Startup.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using MediatR;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -12,6 +13,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Persistence;
+using Application.Activities;
 
 namespace API
 {
@@ -37,6 +39,7 @@ namespace API
                         policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:3000");
                     });
             });
+            services.AddMediatR(typeof(List.Handler).Assembly);
             services.AddControllers();
         }
 
