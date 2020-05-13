2177a178344edc1f7d1ff7f017993d9e14e76f6b
tmorgan-2100 <tmorgan_2100@yahoo.com>
2020-05-13 14:08:34 -0400

    8.5 Added Details Link (pt1)

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -8,6 +8,7 @@ import ActivityStore from '../stores/activityStore';
 import { Route } from 'react-router-dom';
 import HomePage from '../../features/home/HomePage';
 import ActivityForm from '../../features/activities/form/ActivityForm';
+import ActivityDetails from '../../features/activities/details/ActivityDetails';
 
 const App = () => {
   const activityStore = useContext(ActivityStore)
@@ -24,6 +25,7 @@ const App = () => {
       <Container style={{ marginTop: '7em' }}>
               <Route exact path='/' component={HomePage} />
               <Route path='/activities' component={ActivityDashboard} />
+              <Route path='/activities/:id' component={ActivityDetails} />
               <Route path='/createActivity' component={ActivityForm} />
       </Container>
     </Fragment>
