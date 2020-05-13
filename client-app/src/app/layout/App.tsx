5f6b3b7a21751cf612008dde6ff9a21f5ab12cee
tmorgan-2100 <tmorgan_2100@yahoo.com>
2020-05-13 13:57:47 -0400

    8.3 Added Routes

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -5,6 +5,9 @@ import ActivityDashboard from '../../features/activities/dashboard/ActivityDashb
 import LoadingComponent from './LoadingComponent';
 import {observer} from 'mobx-react-lite';
 import ActivityStore from '../stores/activityStore';
+import { Route } from 'react-router-dom';
+import HomePage from '../../features/home/HomePage';
+import ActivityForm from '../../features/activities/form/ActivityForm';
 
 const App = () => {
   const activityStore = useContext(ActivityStore)
@@ -19,7 +22,9 @@ const App = () => {
     <Fragment>
       <NavBar />
       <Container style={{ marginTop: '7em' }}>
-        <ActivityDashboard />
+              <Route exact path='/' component={HomePage} />
+              <Route path='/activities' component={ActivityDashboard} />
+              <Route path='/createActivity' component={ActivityForm} />
       </Container>
     </Fragment>
   );
